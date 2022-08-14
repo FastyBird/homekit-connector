46e823ace132e59a96de1d448ca3fe5d7f860573
Adam Kadlec <adam.kadlec@gmail.com>
2022-08-14 12:03:47 +0200

    Updating dependencies

--- fastybird_homekit_connector/__init__.py
+++ fastybird_homekit_connector/__init__.py
@@ -20,7 +20,7 @@ HomeKit connector module
 
 from .bootstrap import create_connector
 
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 
 __all__ = ["connector", "bootstrap", "__connector_version__"]
 
